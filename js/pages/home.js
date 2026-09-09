27ddeda391233271c7231b0398e377fa258deeba
tongjiehui <54839425+tongjiehui@users.noreply.github.com>
2026-09-09 16:03:44 +0800

    update js/pages/home.js

--- js/pages/home.js
+++ js/pages/home.js
@@ -1,8 +1,8 @@
 // =====================
 // 首页
 // =====================
-import * as store from '../storage.js?v=20260909q';
-import { fmtRelative, setView } from '../ui.js?v=20260909q';
+import * as store from '../storage.js?v=20260909s';
+import { fmtRelative, setView } from '../ui.js?v=20260909s';
 
 export function renderHome() {
   const data = store.getAll();
